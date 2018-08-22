caaa46d9c5cc49055c5b04981ce62752e01c9f4e
Alexandr <admin@sccraft.ru>
2018-08-22 18:09:28 +0300

    ScAsk

--- app/src/main/java/ru/sccraft/scask/NetGet.java
+++ app/src/main/java/ru/sccraft/scask/NetGet.java
@@ -81,21 +81,4 @@ public class NetGet {
         }
         return result;
     }
-
-    public static boolean getNetworkConnectionStatus(final Context context) {
-        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (wifiInfo != null && wifiInfo.isConnected()) {
-            return true;
-        }
-        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (wifiInfo != null && wifiInfo.isConnected()) {
-            return true;
-        }
-        wifiInfo = cm.getActiveNetworkInfo();
-        if (wifiInfo != null && wifiInfo.isConnected()) {
-            return true;
-        }
-        return false;
-    }
 }
