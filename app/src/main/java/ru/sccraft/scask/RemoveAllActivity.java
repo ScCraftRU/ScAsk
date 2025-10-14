7ff798ccc0909debaa4c8fde03ef0b3ec28da68c
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-10-14 18:14:32 +0300

    UI fix

--- app/src/main/java/ru/sccraft/scask/RemoveAllActivity.java
+++ app/src/main/java/ru/sccraft/scask/RemoveAllActivity.java
@@ -3,6 +3,8 @@ package ru.sccraft.scask;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.PersistableBundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ public class RemoveAllActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_remove_all);
         setTitle(getString(R.string.pleaseWait));
         if (r == null) {
