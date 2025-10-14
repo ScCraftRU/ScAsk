a334e18c6d7373d93eef8618f535d99d69cc1c39
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-10-14 18:47:16 +0300

    UI fix

--- app/src/main/java/ru/sccraft/scask/HelpActivity.java
+++ app/src/main/java/ru/sccraft/scask/HelpActivity.java
@@ -1,5 +1,6 @@
 package ru.sccraft.scask;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_help);
     }
 }
